6b81a335bb3106d8d2003ae886be96b04c26cf52
dakimura <dakimura@example.com>
2018-05-20 05:44:43 +0900

    python 2.x support

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from io import open
 from setuptools import setup, find_packages
 
 with open('README.md', encoding="utf-8") as f:
@@ -10,7 +11,7 @@ with open('LICENSE', encoding="utf-8") as f:
 
 setup(
     name='coincheck_api',
-    version='0.6.0',
+    version='0.7.0',
     description='A simple library to use CoinCheck API',
     long_description=readme,
     author='dakimura',
